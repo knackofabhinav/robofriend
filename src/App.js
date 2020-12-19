c56d0d1bab87d264918a332a64b335e1940b2d79
Abhinav Niranjan <66178038+knackofabhinav@users.noreply.github.com>
2020-12-19 06:00:42 +0530

    added Scroll Feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
-import {robots} from "./robots";
+import Scroll from './Scroll';
 import './App.css'
 
 
@@ -29,13 +29,20 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots = {filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className='tc'>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots = {filteredRobots}/>
+                    </Scroll>
+                </div>
+            );
+        }
+        
     }
     
 }
